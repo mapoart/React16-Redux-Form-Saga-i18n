4882019e39bb7b98a540a0ab4901e967989852bc
Marcin Polak <mapoart@gmail.com>
2021-04-23 10:56:03 +0200

    removed depracated warnings

--- services/src/server/index.js
+++ services/src/server/index.js
@@ -1,50 +1,56 @@
-import express from 'express'
-import expressValidation from 'express-validation'
-import bodyParser from 'body-parser'
-import helmet from 'helmet'
-import logger from 'morgan'
+import express from "express";
+import expressValidation from "express-validation";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import logger from "morgan";
 
-import userRoutes from './../modules/user/user.routes'
+import userRoutes from "./../modules/user/user.routes";
 
-var env = process.env.NODE_ENV || 'development'
-var config = require('./config')[env]
+var env = process.env.NODE_ENV || "development";
+var config = require("./config")[env];
 
-const app = express()
+const app = express();
 
 /** Must be both for use and options !!! otherwise this won't work with react !!! */
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With') 
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+  );
+  next();
+});
 
-app.options('/*', function (req, res, next) {
+app.options("/*", function (req, res, next) {
   // No 'Access-Control-Allow-Origin' header is present on the requested resource
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, Content-Length, X-Requested-With')
-  res.sendStatus(200)
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers, Authorization, Content-Length, X-Requested-With"
+  );
+  res.sendStatus(200);
+});
 
-app.enable('etag') // use strong etags
+app.enable("etag"); // use strong etags
 
-var morgan = require('morgan')
-app.use(logger('dev')) // Log all requests to console
+var morgan = require("morgan");
+app.use(logger("dev")); // Log all requests to console
 
 // SECURITY !!!
 // Adds some security https://github.com/helmetjs/helmet app.use(helmet())
-app.use(helmet())
+app.use(helmet());
 
-var cookieParser = require('cookie-parser')
-app.use(cookieParser()) // Read cookies for auth
+var cookieParser = require("cookie-parser");
+app.use(cookieParser()); // Read cookies for auth
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // mount all routes on /api path
-app.use('/users', userRoutes)
+app.use("/users", userRoutes);
 
 // app.use((err, req, res, next) => {
 //   if (err instanceof
@@ -60,8 +66,10 @@ app.use('/users', userRoutes)
 //   }
 // });
 
-app.listen(config.server.port, config.server.host)
-console.log(`WEB: ${config.server.host}:${config.server.port}, DB: ${config.database.name}`)
+app.listen(config.server.port, config.server.host);
+console.log(
+  `WEB: ${config.server.host}:${config.server.port}, DB: ${config.database.name}`
+);
 
 // export default app;
-module.exports = app
+module.exports = app;
